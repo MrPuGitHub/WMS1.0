1307faf826d5b26189306d22ebd9bcbc65de3b58
wudnegyu <1654805021@qq.com>
2019-08-29 10:37:55 +0800

    wudnegyu

---
+++ src/main/java/com/ccc/demoboot/domain/Emp.java
@@ -0,0 +1,37 @@
+package com.ccc.demoboot.domain;
+
+import javax.persistence.Id;
+
+public class Emp {
+
+    @Id
+    private Integer id;
+
+    private String empname;
+
+    private Integer empid;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getEmpname() {
+        return empname;
+    }
+
+    public void setEmpname(String empname) {
+        this.empname = empname;
+    }
+
+    public Integer getEmpid() {
+        return empid;
+    }
+
+    public void setEmpid(Integer empid) {
+        this.empid = empid;
+    }
+}
