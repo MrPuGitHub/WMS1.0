f2144f3a3761de931dcf9b9b6d7946970f1f5f39
wudnegyu <1654805021@qq.com>
2019-08-29 10:35:58 +0800

    wudnegyu

--- src/main/java/com/ccc/demoboot/service/UserService.java
+++ src/main/java/com/ccc/demoboot/service/UserService.java
@@ -3,19 +3,6 @@ package com.ccc.demoboot.service;
 import com.ccc.demoboot.domain.User;
 public interface UserService{
 
-
-    int deleteByPrimaryKey(Integer id);
-
-    int insert(User record);
-
-    int insertSelective(User record);
-
-    User selectByPrimaryKey(Integer id);
-
-    int updateByPrimaryKeySelective(User record);
-
-    int updateByPrimaryKey(User record);
-
     User checkLogin(String username,String password);
 
 }
