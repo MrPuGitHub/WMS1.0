1214cdb49ea405bdb5635614feb8612f8f086f45
kluzhang <zhangkanglu@foxmail.com>
2019-08-30 15:58:11 +0800

    入库界面的修改，路径的调整

--- src/main/java/com/ccc/demoboot/Controller/UserController.java
+++ src/main/java/com/ccc/demoboot/Controller/UserController.java
@@ -1,4 +1,4 @@
-package com.ccc.demoboot.controller;
+package com.ccc.demoboot.Controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
