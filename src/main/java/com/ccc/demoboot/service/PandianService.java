ccb034a8797b5c69794b542b2d3dff206b4baf70
weiwei <1972698267@qq.com>
2019-09-02 19:32:08 +0800

    111

---
+++ src/main/java/com/ccc/demoboot/service/PandianService.java
@@ -0,0 +1,12 @@
+package com.ccc.demoboot.service;
+
+import com.ccc.demoboot.domain.Pandian;
+
+import java.util.List;
+
+public interface PandianService{
+
+
+List<Pandian> selectAllPanDian();
+
+}
---
+++ src/main/java/com/ccc/demoboot/service/PandianServiceImpl.java
@@ -0,0 +1,22 @@
+package com.ccc.demoboot.service;
+
+import org.springframework.stereotype.Service;
+import javax.annotation.Resource;
+import com.ccc.demoboot.mapper.PandianMapper;
+import com.ccc.demoboot.domain.Pandian;
+import com.ccc.demoboot.service.PandianService;
+
+import java.util.List;
+
+@Service
+public class PandianServiceImpl implements PandianService{
+
+    @Resource
+    private PandianMapper pandianMapper;
+
+
+    @Override
+    public List<Pandian> selectAllPanDian() {
+        return pandianMapper.selectAll();
+    }
+}
